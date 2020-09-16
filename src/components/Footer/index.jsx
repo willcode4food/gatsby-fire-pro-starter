d23f4f49d2c3a8f8ef1b9ef10462dd6503de0cbc
Marc Arbesman <marbesman@gmail.com>
2020-09-16 10:45:29 -0400

    footer uses all styled components

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,106 +1,41 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { css } from '@emotion/core'
-import { COLORS, SPACERS, FONT_SIZES, MQ } from 'utils/styleHelpers'
+import { COLORS } from 'utils/styleHelpers'
 import SocialLinks from 'components/SocialLinks'
 import Logo from 'components/Header/Logo'
 import { Breakpoint } from 'react-socks'
-import { FooterHeader, LogoWrapper, CopyrightText } from './styles'
+import {
+	CopyrightText,
+	FooterContentWrapper,
+	FooterContentBoxLogo,
+	FooterContentBoxTermsAndPolicy,
+	FooterContentCopyrightWrapper,
+	FooterLogoWrapper,
+	FooterWrapper,
+	FooterWrapperBox,
+} from './styles'
 
 const Footer = () => {
 
 	return (
-		<div
-			css={css`
-				display: flex;
-				flex-direction: column;
-				justify-content: flex-start;
-				background: ${COLORS.BLACK};
-				transition: background 0.5s;
-				.dark & {
-					background: ${COLORS.DARK_BLACK};
-				}
-				${MQ.L} {
-					flex-direction: column;
-				    justify-content: flex-start;
-				}
-			`}
-		>
-			<div
-                css={css`
-                    flex-basis: 50%;
-					padding-top: ${SPACERS.XL};
-					margin-top: ${SPACERS.L};
-					${MQ.L} {
-						padding-top: ${SPACERS.L};
-						margin-top: ${SPACERS.L};
-					}
-				`}
-			>
-                <div css={css`
-                    display: flex;
-                    flex-direction: row;
-                `}>
-					<div
-						css={css`
-							margin-left: ${SPACERS.M};
-							${MQ.L} {
-								margin-left: 0;
-							}
-						`}
-					>
-                        <div css={css`
-                            display: flex;
-                            flex-direction: column;
-                        `}>
-							<FooterHeader>Temp</FooterHeader>
-							
-						</div>
-					</div>
-				</div>
-			</div>
-			<div>
-				<div css={css`
-                    display: flex;
-                    flex-direction: column;
-                `}>
-					<div
-						css={css`
-							margin-left: ${SPACERS.M};
-							margin-right: ${SPACERS.M};
-							margin-top: ${SPACERS.L};
-							${MQ.L} {
-								margin-top: ${SPACERS.L};
-							}
-						`}
-					>
-						<LogoWrapper>
+		<FooterWrapper>
+			<FooterWrapperBox>
+				<FooterContentWrapper>
+					<FooterContentBoxLogo>
+						<FooterLogoWrapper>
 							<Logo color={COLORS.WHITE} textColor={COLORS.WHITE} />
-						</LogoWrapper>
-					</div>
-
+						</FooterLogoWrapper>
+					</FooterContentBoxLogo>
 					<div>
 						<Breakpoint M up>
 							<SocialLinks />
 						</Breakpoint>
 					</div>
-					<div
-						css={css`
-							margin-left: ${SPACERS.M};
-							font-size: ${FONT_SIZES.H7};
-							color: ${COLORS.GREEN};
-						`}
-					>
+					<FooterContentBoxTermsAndPolicy>
 						<Link to="/tos"> Terms of Use</Link> | <Link to="/privacy-policy">Privacy Policy</Link>
-					</div>
+					</FooterContentBoxTermsAndPolicy>
 					<div>
-						<div
-                            css={css`
-                                display: flex;
-                                flex-direction: row;
-                                justify-content: space-between;
-								margin: ${SPACERS.M};
-							`}>
+						<FooterContentCopyrightWrapper>
 							<div>
 								<CopyrightText>Copyright 2019 All Rights Researved</CopyrightText>
 							</div>
@@ -109,11 +44,11 @@ const Footer = () => {
 									<SocialLinks />
 								</div>
 							</Breakpoint>
-						</div>
+						</FooterContentCopyrightWrapper>
 					</div>
-				</div>
-			</div>
-		</div>
+				</FooterContentWrapper>
+			</FooterWrapperBox>
+		</FooterWrapper>
 	)
 }
 
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,13 +1,57 @@
 import styled from '@emotion/styled'
-import { COLORS, SPACERS, FONT_SIZES } from 'utils/styleHelpers'
+import { COLORS, SPACERS, FONT_SIZES, MQ } from 'utils/styleHelpers'
 import { Link } from 'gatsby'
 
-export const FooterHeader = styled.div`
-	color: ${COLORS.GREEN};
-	font-size: ${FONT_SIZES.H3};
-	font-weight: 600;
-	margin-bottom: ${SPACERS.M};
+
+export const FooterWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	background: ${COLORS.BLACK};
+	transition: background 0.5s;
+	.dark & {
+		background: ${COLORS.DARK_BLACK};
+	}
+	${MQ.L} {
+		flex-direction: column;
+		justify-content: flex-start;
+	}
 `
+export const FooterLogoWrapper = styled.div`
+	height: 32px;
+	width: 194px;
+`
+
+export const FooterWrapperBox = styled.div`
+	width: 20%;
+`
+export const FooterContentWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+`
+
+export const FooterContentBoxLogo = styled.div`
+	margin-left: ${SPACERS.M};
+	margin-right: ${SPACERS.M};
+	margin-top: ${SPACERS.L};
+	${MQ.L} {
+		margin-top: ${SPACERS.L};
+	}
+`
+
+export const FooterContentBoxTermsAndPolicy = styled.div`
+	margin-left: ${SPACERS.M};
+	font-size: ${FONT_SIZES.H7};
+	color: ${COLORS.GREEN};
+`
+
+export const FooterContentCopyrightWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	margin: ${SPACERS.M};
+`
+
 export const FooterLinkBox = styled.div`
 	white-space: nowrap;
 	overflow: hidden;
@@ -18,10 +62,6 @@ export const FooterLinkBox = styled.div`
 export const FooterLink = styled(Link)`
 	color: ${COLORS.LIGHT_GRAY};
 	font-size: ${FONT_SIZES.H5};
-`
-export const LogoWrapper = styled.div`
-	height: 80px;
-	width: 160px;
 `
 export const CopyrightText = styled.p`
 	color: ${COLORS.MEDIUM_GRAY};
