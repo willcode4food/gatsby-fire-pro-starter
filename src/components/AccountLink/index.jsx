c93168ac96335f180ee47bd75e11688b3b10ed66
Marc Arbesman <marbesman@gmail.com>
2020-10-15 10:44:08 -0400

    first round to make sure it works in vscode formatter (prettier)

--- src/components/AccountLink/index.jsx
+++ src/components/AccountLink/index.jsx
@@ -5,14 +5,14 @@ import useAuthorization from 'hooks/useAuthorization'
 import { AccountCircle } from './styles'
 
 function AccountLink() {
-	const { isAuthorized, isLoading } = useAuthorization(authConditionIsUser)
-	if (!isLoading && isAuthorized) {
-		return (
-			<Link to={'/account'}>
-				<AccountCircle />
-			</Link>
-		)
-	}
-	return null
+    const { isAuthorized, isLoading } = useAuthorization(authConditionIsUser)
+    if (!isLoading && isAuthorized) {
+        return (
+            <Link to={'/account'}>
+                <AccountCircle />
+            </Link>
+        )
+    }
+    return null
 }
 export default AccountLink
--- src/components/AccountProfileForm/styles.js
+++ src/components/AccountProfileForm/styles.js
@@ -2,19 +2,19 @@ import styled from '@emotion/styled'
 import { SPACERS } from 'utils/styleHelpers'
 
 export const ImageEditorControlsWrapper = styled.div`
-	display: flex;
-	justify-content: space-evenly;
-	width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
 `
 
 export const ImageEditorControlsBox = styled.div``
 
 export const ImageEditorControlsCenterBox = styled.div`
-	flex-basis: 60%;
+    flex-basis: 60%;
 `
 export const AvatarEditorBox = styled.div`
-	box-sizing: border-box;
-	width: 100%;
-	margin: ${SPACERS.S};
-	text-align: center;
+    box-sizing: border-box;
+    width: 100%;
+    margin: ${SPACERS.S};
+    text-align: center;
 `
