7fbebcbfa27323df6e4de61087b13aa68c62feab
Marc Arbesman <marbesman@gmail.com>
2020-10-18 10:48:15 -0400

    adds test for email submit button removes uneeded test in checkbox field

--- src/components/Forms/FormFields/CheckBoxField/index.test.js
+++ src/components/Forms/FormFields/CheckBoxField/index.test.js
@@ -4,7 +4,7 @@ import CheckBoxField from './'
 import { render, fireEvent } from '@testing-library/react'
 import Adapter from 'enzyme-adapter-react-16'
 import { configure, mount } from 'enzyme'
-import { CheckBoxStyle } from './styles'
+
 configure({ adapter: new Adapter() })
 
 jest.mock('utils/styleHelpers', () => {
@@ -17,58 +17,8 @@ jest.mock('utils/styleHelpers', () => {
 
 describe('CheckBoxField', () => {
     describe('when checkbox is added', () => {
-        it('renders unchecked', () => {
+        it('renders', () => {
             const tree = renderer.create(<CheckBoxField backgroundColor="tomato" backgroundColorOff="blue" />).toJSON()
-            expect(tree).toMatchInlineSnapshot(`
-                .emotion-2 {
-                  -webkit-appearence: button;
-                  -moz-appearence: button;
-                  appearence: button;
-                  border: 1px solid;
-                  border-radius: 1px;
-                  cursor: pointer;
-                  display: block;
-                  margin: 5px 10px;
-                  padding: 5px 10px;
-                  width: 25px;
-                  height: 25px;
-                }
-
-                .emotion-2:after {
-                  content: '✓';
-                  font-size: 25px;
-                  color: #c60028;
-                }
-
-                .emotion-2:before {
-                  content: '';
-                }
-
-                .emotion-0 {
-                  position: absolute;
-                  left: -9999px;
-                }
-
-                .emotion-0:checked + label {
-                  color: red;
-                }
-
-                <label
-                  className="emotion-2 emotion-3"
-                >
-                  <input
-                    className="emotion-0 emotion-1"
-                    onClick={[Function]}
-                    role="checkbox"
-                    type="checkbox"
-                  />
-                </label>
-            `)
-        })
-        it('renders checked', () => {
-            const tree = renderer
-                .create(<CheckBoxField backgroundColor="tomato" backgroundColorOff="blue" defaultIsChecked={true} />)
-                .toJSON()
             expect(tree).toMatchInlineSnapshot(`
                 .emotion-2 {
                   -webkit-appearence: button;
@@ -123,7 +73,7 @@ describe('CheckBoxField', () => {
             fireEvent.click(checkBox)
             expect(checkBox.checked).toEqual(true)
         })
-        it('changes state to inverse of state', () => {
+        it('executes useState when clicked', () => {
             const setIsChecked = jest.fn()
             const wrapper = mount(<CheckBoxField />)
             const useStateSpy = jest.spyOn(React, 'useContext')
