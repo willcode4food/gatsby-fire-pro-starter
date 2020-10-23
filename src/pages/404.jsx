f2fbe24cd1e6ced92a3482aae7b587033640b5ba
Marc Arbesman <marbesman@gmail.com>
2020-10-22 22:07:32 -0400

    404 overides modal

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import GlobalLayout from 'components/GlobalLayout'
 
 const NotFoundPage = ({ location }) => (
-    <GlobalLayout location={location}>
+    <GlobalLayout location={location} isVerifyAgeModalOverriden={true}>
         <h1>NOT FOUND</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </GlobalLayout>
